abc2df186de76b31ed88470e29bb00a563d0e395
Khaleel Jageer <jskcse4@gmail.com>
2021-05-05 22:47:12 +0530

    sqlite generated

--- update_strapi.py
+++ update_strapi.py
@@ -1,21 +1,26 @@
+import sqlite3
 import json
 import requests
 import codecs
+import time
 
 file = codecs.open('data_tech.json', 'r', 'utf-8-sig')
 j_data = json.load(file)
 
-headers = {
-    'accept' : 'application/json',
-    'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjE5NjcyMjcxLCJleHAiOjE2MjIyNjQyNzF9.TGyJ-KcVx2_Gxo6utmnK5d_DkqBDcMUa6TNN25zI6is',
-    'Content-Type' : 'application/json'
-}
+conn = sqlite3.connect('glossary.db')
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE tamil_glossary
+             ([id] INTEGER PRIMARY KEY, [subject] text, [en_term] text, [ta_term] text)''')
 
-for item in j_data['tamil_glossary']:
-    data = {
-        'subject' : item['subject'],
-        'en_term' : item['en_term'],
-        'ta_term' : item['ta_term']
-    }
-    r = requests.post('http://localhost:1337/tamil-glossaries', data= json.dumps(data, ensure_ascii=False).encode('utf-8'), headers=headers)
-    print(r.status_code)
+for i, item in enumerate(j_data['tamil_glossary']):
+    id = int(time.time() * 1000)
+    subject = item['subject']
+    en_term = item['en_term']
+    ta_term = item['ta_term']
+    print('inserting...%d' % i)
+    query = "INSERT INTO tamil_glossary ('id', 'subject', 'en_term', 'ta_term') VALUES (?, ?, ?, ?);"
+    value = (id, subject, en_term, ta_term)
+    cursor.execute(query, value)
+    conn.commit()
+
+conn.close()
